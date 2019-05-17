2b5dfdd1a23d2e7a722c89ac69f68878204996aa
Anelisy Nagano <anelisynagano@gmail.com>
2019-05-17 15:02:22 +0100

    fix howto

--- src/HowToPlay.jsx
+++ src/HowToPlay.jsx
@@ -7,7 +7,7 @@ const HowToPlay = () => {
   return (
     <div>
       <h1>Game Rules</h1>
-      <p>The game is played where players deliver hand signals that will represent the elements of the game; rock, paper and scissors. The outcome of the game is determined by 3 simple rules:</p>
+      <p>The game is played when players deliver hand signals that will represent the elements of the game; rock, paper and scissors. The outcome of the game is determined by 3 simple rules:</p>
         <ul>
           <li> Rock wins against scissors.</li>
           <li> Scissors win against paper.</li>
