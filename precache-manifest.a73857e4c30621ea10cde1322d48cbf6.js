8063acc30573c8d2da4ca0f04c192f96c8f2ba82
Anelisy Nagano <anelisynagano@gmail.com>
2019-05-17 12:46:10 +0100

    Updates

---
+++ precache-manifest.a73857e4c30621ea10cde1322d48cbf6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758bc525c8a5ee513f85e674de345e45",
+    "url": "/hackathon1/index.html"
+  },
+  {
+    "revision": "f8fa5bb957053d09bb5c",
+    "url": "/hackathon1/static/css/main.734ab467.chunk.css"
+  },
+  {
+    "revision": "d647f8838c2a1d99c48d",
+    "url": "/hackathon1/static/js/2.a4e108dd.chunk.js"
+  },
+  {
+    "revision": "f8fa5bb957053d09bb5c",
+    "url": "/hackathon1/static/js/main.2f0f4232.chunk.js"
+  },
+  {
+    "revision": "ba3f61873ed0db88353e",
+    "url": "/hackathon1/static/js/runtime~main.d2c61f40.js"
+  }
+]);
